ebb9681418a877b115ea10311d3c3404bd7434ea
alyona-ra <70214004+alyona-ra@users.noreply.github.com>
2021-05-17 14:38:18 -0400

    Add files via upload

--- Afford It NYC/js/open_images.js
+++ Afford It NYC/js/open_images.js
@@ -1,41 +1,24 @@
-document.querySelectorAll('.help-container img').forEach((img) => {
-    img.style.height = `${document.querySelectorAll('.help-container img')[0].offsetHeight}px`;
-  });
-  
-  document.querySelectorAll('.help-container img').forEach((el) => {
-    el.addEventListener('click', openModal);
-  });
-  
-  window.onresize = () => {
-    document.querySelectorAll('.help-container img').forEach((img) => {
-      img.style.display = 'none';
-    });
-  
-    document.querySelectorAll('.help-container img').forEach((img) => {
-      img.style.height = `${
-        document.querySelectorAll('.help-container')[0].offsetHeight
-      }px`;
-      img.style.display = '';
-    });
-  };
-  
+document.querySelector('.help-container img').addEventListener('click', openModal);
+
   function openModal(e) {
     let body = document.querySelector('body');
     let modal = document.createElement('div');
     let modalImg = document.createElement('img');
   
-    body.style.overflow = 'hidden';
-    modal.classList.add('modal');
-    modal.style.top = `${window.scrollY}px`;
-    modal.addEventListener('click', closeModal);
-    modalImg.src = `${e.target.getAttribute('src')}`;
+    if (window.innerWidth > 420) {
+      body.style.overflow = 'hidden';
+      modal.classList.add('modal');
+      modal.style.top = `${window.scrollY}px`;
+      modal.addEventListener('click', closeModal);
+      modalImg.src = `${e.target.getAttribute('src')}`;
   
-    modal.appendChild(modalImg);
-    body.appendChild(modal);
+      modal.appendChild(modalImg);
+      body.appendChild(modal);
+    }
   }
   
   function closeModal(e) {
-    if (e.target.nodeName == 'DIV') {
+    if (e.target.nodeName == 'DIV' && window.innerWidth > 420) {
       document.querySelector('.modal').remove();
       document.querySelector('body').style.overflow = '';
     }
